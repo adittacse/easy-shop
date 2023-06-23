196650d74a1cc628cecda73c0fbd1c1112c0b2ce
Aditta Chakraborty <adittacse@gmail.com>
2023-06-24 03:09:05 +0600

    mongodb connected & product categories loaded

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSlider from "@/app/(home)/HeroSlider";
+import Categories from "@/app/(home)/Categories";
 
 const HomePage = () => {
     return (
         <main>
             <HeroSlider></HeroSlider>
+            <Categories></Categories>
         </main>
     );
 };
---
+++ src/services/category.service.js
@@ -0,0 +1,9 @@
+import "server-only";
+import DbConnect from "./DbConnect";
+import 'server-only';
+
+export const getCategoriesFromDb = async () => {
+    const db = await DbConnect();
+    const categoriesCollection = db.collection("categories");
+    return categoriesCollection.find({}).toArray();
+};
